f14b384cd9a8ff7014d64ee2ec4ad8d8edd46162
Kirankumar <kirannatarajan26@gmail.com>
2022-07-25 16:35:53 +0530

    calndr

--- front/src/Components/Weather/Styles.ts
+++ front/src/Components/Weather/Styles.ts
@@ -9,50 +9,71 @@ export const useStyles = makeStyles({
     bgcolor: "background.paper",
     paddingTop: "16px!important",
     paddingBottom: "16px!important",
-    "& > li": {
-      width: "33.3%",
-      float: "left",
-      textAlign: "center",
-    },
+    // "& > li": {
+    //   width: "33.3%",
+    //   float: "left",
+    //   textAlign: "center",
+    // },
   },
-  weather: {
-    display: "inline-block !important",
-    paddingTop: "0px!important",
-    paddingBottom: "0!important",
-    "& > h4": {
-      fontSize: "12px",
-      marginBottom: "10px",
-    },
-    "& > h1": {
-      fontSize: "26px",
-      color: "#009BAD",
-      marginBottom: "5px",
-      lineHeight: "26px",
-    },
-    "& > div": {
-     
-      lineHeight: "13px",
-      paddingTop: "3px",
-    },
+
+  Calendar: {
+    border: "white",
+    paddingLeft:"12px",
+    borderBottom:"1px solid #F1F1F1",
+    paddingBottom:"20px",
+    margin:"auto",
   },
-  currency: {
-    display: "inline-block !important",
-    paddingTop: "0px!important",
-    paddingBottom: "0!important",
-    "& > h4": {
-      fontSize: "12px",
-      marginBottom: "10px",
-    },
-    "& > h1": {
-      fontSize: "26px",
-      color: "#009BAD",
-      marginBottom: "5px",
-      lineHeight: "26px",
-    },
-    "& > div": {
-      lineHeight: "13px",
-      paddingTop: "3px",
-     
-    },
+  calenderHeader: {
+    fontSize: "1rem !important",
+    color: "#009BAD !important",
+    marginBottom: "12px !important",
   },
+  calenderHeader2: {
+    fontSize: "0.8rem !important",
+    color: "#009BAD !important",
+    marginBottom: "12px !important",
+  },
+  border:{
+    border:"none",
+  },
+  // weather: {
+  //   display: "inline-block !important",
+  //   paddingTop: "0px!important",
+  //   paddingBottom: "0!important",
+  //   "& > h4": {
+  //     fontSize: "12px",
+  //     marginBottom: "10px",
+  //   },
+  //   "& > h1": {
+  //     fontSize: "26px",
+  //     color: "#009BAD",
+  //     marginBottom: "5px",
+  //     lineHeight: "26px",
+  //   },
+  //   "& > div": {
+
+  //     lineHeight: "13px",
+  //     paddingTop: "3px",
+  //   },
+  // },
+  // currency: {
+  //   display: "inline-block !important",
+  //   paddingTop: "0px!important",
+  //   paddingBottom: "0!important",
+  //   "& > h4": {
+  //     fontSize: "12px",
+  //     marginBottom: "10px",
+  //   },
+  //   "& > h1": {
+  //     fontSize: "26px",
+  //     color: "#009BAD",
+  //     marginBottom: "5px",
+  //     lineHeight: "26px",
+  //   },
+  //   "& > div": {
+  //     lineHeight: "13px",
+  //     paddingTop: "3px",
+
+  //   },
+  // },
 });
--- front/src/Components/Weather/index.tsx
+++ front/src/Components/Weather/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable */
-import * as React from "react";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
@@ -9,25 +10,28 @@ import Avatar from "@mui/material/Avatar";
 // import WorkIcon from "@mui/icons-material/Work";
 // import BeachAccessIcon from "@mui/icons-material/BeachAccess";
 import Divider from "@mui/material/Divider";
-import { Paper, Typography } from "@mui/material";
+import { Paper, Stack, Typography } from "@mui/material";
 import { useStyles } from "./Styles";
+import "react-calendar/dist/Calendar.css";
+import SkeletonAnimation from "../../Containers/Skeleton";
+import { useGetQuickLinksQuery,useUpdateQuicklinkTokenMutation,useGetAllQuickLinkQuery } from '../../services/APIs';
 
 export default function Weather() {
   var classes = useStyles();
-
+  const [value, onChange] = useState(new Date());
+  const [token, setToken] = useState<string>();
+  const { data, error, isLoading } = useGetAllQuickLinkQuery(token);
   return (
     <Paper elevation={0}>
+      {isLoading ? (
+          <SkeletonAnimation />
+        ) : (
       <List className={classes.root}>
-        <ListItem className={classes.weather}>
-          <Typography component="h4">Dubai, UAE</Typography>
+        {/*  <ListItem className={classes.weather}>
+          <Typography component="h4">kkjhjkhjkh, UAE</Typography>
           <Typography component="h1">32.01C</Typography>
 
-          <Typography
-            variant="caption"
-           
-            component="div"
-            sx={{ opacity: 0.6 }}
-          >
+          <Typography variant="caption" component="div" sx={{ opacity: 0.6 }}>
             Sunny
           </Typography>
         </ListItem>
@@ -36,12 +40,7 @@ export default function Weather() {
           <Typography component="h4">Next Prayer</Typography>
           <Typography component="h1">16:45</Typography>
 
-          <Typography
-            variant="caption"
-         
-            component="div"
-            sx={{ opacity: 0.6 }}
-          >
+          <Typography variant="caption" component="div" sx={{ opacity: 0.6 }}>
             Asher
           </Typography>
         </ListItem>
@@ -50,16 +49,44 @@ export default function Weather() {
           <Typography component="h4">1.00 AED is</Typography>
           <Typography component="h1"> 0.27 </Typography>
 
-          <Typography
-            variant="caption"
-          
-            component="div"
-            sx={{ opacity: 0.6 }}
-          >
+          <Typography variant="caption" component="div" sx={{ opacity: 0.6 }}>
             USD
           </Typography>
-        </ListItem>
+        </ListItem> */}
+
+        <div className={classes.Calendar}>
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            spacing={2}
+          >
+            <Typography
+              className={classes.calenderHeader}
+              variant="h6"
+              component="h6"
+              color="secondary"
+            >
+              Events Calendar
+            </Typography>
+
+            <Typography
+              className={classes.calenderHeader2}
+              variant="h6"
+              component="h1"
+              color="secondary"
+            >
+              View All Events
+            </Typography>
+          </Stack>
+          <Calendar
+            className={classes.border}
+            onChange={onChange}
+            value={value}
+          />
+        </div>
       </List>
+       )}
     </Paper>
   );
 }
