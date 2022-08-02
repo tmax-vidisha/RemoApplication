7f2224b555256bff902b987f2bf500e69a923c72
Vidisha Arkasali <vidisha.a@tmax.in>
2022-08-02 13:28:23 +0530

    social

--- front/src/Components/SocialMedia/index.tsx
+++ front/src/Components/SocialMedia/index.tsx
@@ -7,51 +7,55 @@ import FacebookIcon from "../../Assets/Images/s1.svg";
 import InstagramIcon from "../../Assets/Images/s2.svg";
 import TwitterIcon from "../../Assets/Images/s3.svg";
 import LinkedInIcon from "../../Assets/Images/s4.svg";
-
+import { FacebookEmbed, LinkedInEmbed, InstagramEmbed } from 'react-social-media-embed';
 
 interface TabPanelProps {
-    children?: React.ReactNode;
-    index: any;
-    value: any;
-  }
-  
-  function TabPanel(props: TabPanelProps) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`scrollable-prevent-tabpanel-${index}`}
-        aria-labelledby={`scrollable-prevent-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box p={0}>
-            <Typography component="span" variant="body2">
-              {children}
-            </Typography>
-          </Box>
-        )}
-      </div>
-    );
-  }
-  
-  function a11yProps(index: any) {
-    return {
-      id: `scrollable-prevent-tab-${index}`,
-      "aria-controls": `scrollable-prevent-tabpanel-${index}`,
-    };
-  }
-  
+  children?: React.ReactNode;
+  index: any;
+  value: any;
+}
+
+interface type {
+  clientAccessToken: string;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-prevent-tabpanel-${index}`}
+      aria-labelledby={`scrollable-prevent-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={0}>
+          <Typography component="span" variant="body2">
+            {children}
+          </Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: any) {
+  return {
+    id: `scrollable-prevent-tab-${index}`,
+    "aria-controls": `scrollable-prevent-tabpanel-${index}`,
+  };
+}
+
 
 const SocialMedia = () => {
-    const classes = useStyles();
-    const [value, setValue] = React.useState(0);
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
 
-    const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
-        setValue(newValue);
-      };
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
   return (
     // <div>SocialMedia</div>
     <>
@@ -125,21 +129,91 @@ const SocialMedia = () => {
           <Timeline
             dataSource={{
               sourceType: "profile",
-              screenName: "DPWorldUAE",
+              screenName: "remo_digital",
+
             }}
             options={{
               height: "307",
             }}
           />
+          {/* <div id="twitter">
+            <a className="twitter-timeline" href="https://twitter.com/remo_digital">Tweets by Remo Digital</a>
+          </div> */}
         </TabPanel>
         <TabPanel value={value} index={1}>
-          Item Two
+
+          {/* <InstagramEmbed
+            url='https://instagr.am/p/Zw9o4/'
+            clientAccessToken='123|456'
+            maxWidth={320}
+            hideCaption={false}
+            containerTagName='div'
+            protocol=''
+            injectScript
+            onLoading={() => { }}
+            onSuccess={() => { }}
+            onAfterRender={() => { }}
+            onFailure={() => { }}
+          /> */}
+
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+          >
+            <InstagramEmbed
+              //protocol=''
+              //injectScript
+              //onLoading={() => { }}
+              //onSuccess={() => { }}
+              //onAfterRender={() => { }}
+              // onFailure={() => { }}
+              //url="https://www.instagram.com/p/CUbHfhpswxt/"
+              url="https://www.instagram.com/p/CgL9BRHJ4ns/"
+            // clientAccessToken="123|456"
+            //maxWidth={320}
+            //hideCaption={false}
+            //containerTagName='div'
+
+            />
+          </div>
+
         </TabPanel>
         <TabPanel value={value} index={2}>
-          Item Three
+
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <LinkedInEmbed
+              //url="https://www.linkedin.com/embed/feed/update/urn:li:share:6898694772484112384"
+              url="https://www.linkedin.com/embed/feed/update/urn:li:share:6955728295980056577"
+              //url="https://www.linkedin.com/embed/feed/update/urn:li:share:6955091372349542400"
+              //url="https://www.linkedin.com/company/google/?originalSubdomain=in"
+              //postUrl="https://www.linkedin.com/posts/google_interviewing-is-not-one-sided-as-an-aspiring-activity-6954114739874512897-N4OR?utm_source=linkedin_share&utm_medium=member_desktop_web"
+              postUrl="https://www.linkedin.com/embed/feed/update/urn:li:share:6955728295980056577"
+
+              width={325}
+              height={370}
+            />
+            {/* <LinkedinProfile
+              lang="en_US"
+              task="technomax-systems" // Or "SCHOOL_NAME"
+            /> */}
+            {/* <iframe 
+            src="https://www.linkedin.com/embed/feed/update/urn:li:share:6898694772484112384"
+             height="570" width="325"
+             ></iframe> */}
+          </div>
         </TabPanel>
         <TabPanel value={value} index={3}>
-          Item Four
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <FacebookEmbed 
+            //url="https://www.facebook.com/andrewismusic/posts/451971596293956" 
+           //url="https://www.facebook.com/taqeefmiddleeast/posts/" 
+            //url="https://www.facebook.com/taqeefmiddleeast/photos" 
+            url="https://fb.watch/eqiJfx_kmH/" 
+            width={350} />
+          </div>
+          
         </TabPanel>
       </Paper>
     </>
