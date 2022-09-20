6f21cabbf86e10331654072e53bfa6b01343b196
Vidisha Arkasali <vidisha.a@tmax.in>
2022-09-20 13:04:46 +0530

    thds

--- front/src/Components/WorkSpaceOne/MyFilesPage.tsx
+++ front/src/Components/WorkSpaceOne/MyFilesPage.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React,{useState, useReducer } from 'react';
+import { breadcrumbsReducer, foldersReducer } from '../../Store copy/Reducer/foldersReducer';
+import { ActionType } from '../../Store copy/Actions/actionTypes';
 import WPOneDrive from './../Workspace/OneDrive/index';
-import { Grid } from '@mui/material';
+import { Grid, Link } from '@mui/material';
 import { Typography } from '@mui/material';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
@@ -18,15 +20,29 @@ import TableBody from '@mui/material/TableBody';
 import Table from '@mui/material/Table';
 import { Paper } from '@mui/material';
 import useCustom from '../../hooks/useCustom';
-import { useGetAllRootItemsOneDriveQuery } from '../../services/graph';
+import Breadcrumb from '../../hooks/Breadcrumb';
+import { useGetAllRootItemsOneDriveQuery,useGetItemChildrenOneDriveMutation } from '../../services/graph';
 import moment from "moment";
 const MyFilesPage = () => {
     const classes = useStyles();
     const [age, setAge] = React.useState('');
+    const [url,setUrl] = useState<string>('');
+    const [show ,setShow] = useState<boolean>(true)
+    const [s,setS] = useState<boolean>(false)
     const {token} = useCustom();
     //@ts-ignore
-    const { data, error, isLoading } = useGetAllRootItemsOneDriveQuery(token);
+    const {data, error, isLoading}  = useGetAllRootItemsOneDriveQuery(token);
     console.log(data?.response)
+    const [sendItem,{data:ItemChildren,error:itemChildrenError,isLoading:itemChildrenIsLoading}]  = useGetItemChildrenOneDriveMutation();
+    console.log(ItemChildren?.response,'yujujujuys')
+    const [breadcrumbsState, breadcrumbsDispatch] = useReducer(breadcrumbsReducer, {
+        breadcrumbs: [{
+            id: '',
+            name: 'Home'
+        }]
+    });
+    console.log('breadcrumbsState', breadcrumbsState)
+    // sendItem(token)
     const handleChange = (event: SelectChangeEvent) => {
         setAge(event.target.value);
     };
@@ -48,7 +64,51 @@ const MyFilesPage = () => {
         createData('Dream designs', "Jahanara", "August 30 2022", "2 kb", "..."),
 
     ];
+    const setBreadCrumbAction = async (id: string, name: string) => {
+        return breadcrumbsDispatch({ type: ActionType.SET_BREADCRUMBS, payload: { id, name } });
+    };
 
+    const updateBreadCrumbAction = async (id: string) => {
+        return breadcrumbsDispatch({ type: ActionType.UPDATE_BREADCRUMBS, payload: { id } });
+    };
+    const folderClickHandler = async (id: string, name: string,
+        folder:any,webUrl:any
+        ): Promise<void> =>  {
+        
+        // await getFolderChildrenAction(id);
+        // await setBreadCrumbAction(id, name);
+       console.log(id,name)
+       console.log(folder)
+       console.log(webUrl)
+       if(folder === undefined){
+         setUrl(webUrl)
+       }else{
+        console.log('Its  folder')
+        // setShow(!show)
+            const Data = {
+        // name:id,
+        ItemId:id,
+       Name:name
+      }
+     //  console.log(fd)
+        await sendItem(Data)
+        await setBreadCrumbAction(id, name);
+       }
+   
+        
+    // setShow(!show)
+    };
+    const breadcrumbClickHandler = async (id: string) => {
+        await updateBreadCrumbAction(id);
+        // await getFolderChildrenAction(id);
+        const Data = {
+            // name:id,
+            ItemId:id,
+        //    Name:name
+          }
+         //  console.log(fd)
+            await sendItem(Data)
+    };
 
     return (
         <Grid>
@@ -58,7 +118,8 @@ const MyFilesPage = () => {
             <Grid className={classes.bigPart}>
                 <Grid className={classes.myFile}>
                     <Grid>
-
+                    <Breadcrumb breadcrumb={breadcrumbsState.breadcrumbs}
+                                                    getChildHandler={breadcrumbClickHandler} />
                         <FormControl variant="standard" sx={{ m: 1, minWidth: 140 }}>
                             <InputLabel id="demo-simple-select-standard-label"><span>Sort by</span> Newest</InputLabel>
                             <Select
@@ -86,6 +147,7 @@ const MyFilesPage = () => {
                 </Grid>
                 <Stack>
                     <TableContainer component={Paper}>
+                   
                         <Table sx={{ minWidth: 650 }} aria-label="simple table">
                             <TableHead>
                                 <TableRow>
@@ -112,6 +174,8 @@ const MyFilesPage = () => {
                                     </TableRow>
                                 ))}
                             </TableBody> */}
+                            
+                            {show?
                             <TableBody>
                                 {data?.response &&
                                     data?.response.map((item: any, index: any) => {
@@ -129,14 +193,48 @@ const MyFilesPage = () => {
                                         //   var createdDate = moment(
                                         //     item.lastModifiedDateTime
                                         //   ).fromNow();
-
+                                        // let result = (item?.folder === undefined) ? item?.webUrl :'';
+                                        // let result ; 
+                                        // if(item?.folder === undefined){
+                                        //   result= item?.webUrl
+                                        // }
                                         return (
                                             <TableRow
                                                 key={item.name}
                                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                             >
+                                                
                                                 <TableCell component="th" scope="row">
+                                                    {/* <Link 
+                                                    onClick={()=>{
+                                                        // folderClickHandler(item.id,item.name)
+                                                    //  if(item.name.includes('.txt') || item.name.includes('.xlsx') || item.name.includes('.docx')  || item.name.includes('.pptx')){
+                                                        if(item.folder === undefined){
+                                                    //   setUrl(item?.webUrl)
+                                                         console.log(item?.webUrl)
+                                                         
+                                                    }else{
+                                                       
+                                                        //   handleSubmit(item?.folder);
+                                                         console.log(item?.folder)
+                                                         folderClickHandler(item.id,item.name)
+                                                         setShow(!show)
+                                                    }
+                                                }}
+                                                
+                                                    //   href={`${result}`}
+                                                    > */}
+                                                    <Link onClick={()=>{
+                                                        setShow(!show)
+                                                        folderClickHandler(item.id,item.name,item.folder,item?.webUrl)
+                                                        
+                                                    
+                                                    }}
+                                                    // href={`${url}`}
+                                                    >
                                                     {item.name}
+                                                    </Link>
+                                                    
                                                 </TableCell>
                                                 <TableCell align="right">
                                                     {item.lastModifiedBy.user.displayName}
@@ -151,7 +249,82 @@ const MyFilesPage = () => {
                                         )
                                     })}
 
-                            </TableBody>
+                            </TableBody> :  
+                            <TableBody>
+                              
+                            {ItemChildren?.response &&
+                                ItemChildren?.response.map((item: any, index: any) => {
+                                    //   const { fields = {} } = item;
+                                    //   // console.log(fields,'yjyjyjyjyj')
+                                    //   var eventTitle = fields?.Title;
+                                    //   console.log(eventTitle,'yjyjyjyjyj')
+                                    //   var eventStart = moment(fields?.EventDate).format("llll");
+                                    //   var eventDate = moment(fields?.EndDate).format("llll");
+
+                                    //   var eventIsActive = fields.IsActive;
+                                    // let createdMonth = moment(item.lastModifiedDateTime).format("MMM");
+                                    // let createdYear = moment(item.lastModifiedDateTime).format("YYYY");
+                                    let createdDate = moment(item.lastModifiedDateTime).format("DD-MMM-YYYY");
+                                    //   var createdDate = moment(
+                                    //     item.lastModifiedDateTime
+                                    //   ).fromNow();
+                                    // let result = (item?.folder === undefined) ? item?.webUrl :'';
+                                    // let result ; 
+                                    // if(item?.folder === undefined){
+                                    //   result= item?.webUrl
+                                    // }
+                                    return (
+                                        <TableRow
+                                            key={item.name}
+                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                        >
+                                            
+                                            <TableCell component="th" scope="row">
+                                                {/* <Link 
+                                                onClick={()=>{
+                                                    // folderClickHandler(item.id,item.name)
+                                                //  if(item.name.includes('.txt') || item.name.includes('.xlsx') || item.name.includes('.docx')  || item.name.includes('.pptx')){
+                                                    if(item.folder === undefined){
+                                                //   setUrl(item?.webUrl)
+                                                     console.log(item?.webUrl)
+                                                    //  setS(!s)
+                                                }else{
+                                                   
+                                                    //   handleSubmit(item?.folder);
+                                                     console.log(item?.folder)
+                                                     folderClickHandler(item.id,item.name)
+                                                   
+                                                }
+                                            }}
+                                            
+                                                //   href={`${result}`}
+                                                > */}
+                                                <Link onClick={()=>{
+                                                     folderClickHandler(item.id,item.name,item.folder,item?.webUrl)
+                                                     
+                                                }}>
+                                                {item.name}
+                                                </Link>
+                                                
+                                            </TableCell>
+                                            <TableCell align="right">
+                                                {item.lastModifiedBy.user.displayName}
+                                            </TableCell>
+                                            <TableCell align="right">
+                                                {createdDate} 
+                                            </TableCell>
+                                            <TableCell align="right">
+                                                {item.size}
+                                            </TableCell>
+                                        </TableRow>
+                                    )
+                                })}
+
+                        </TableBody> 
+                            
+                            
+                            }
+                            
                         </Table>
                     </TableContainer>
                 </Stack>
