c0629b59a676362cb9f3fba2fcd35b15148ece55
Vidisha Arkasali <vidisha.a@tmax.in>
2022-09-23 17:34:57 +0530

    y

--- front/src/Components/WorkSpaceOne/MyFilesPage.tsx
+++ front/src/Components/WorkSpaceOne/MyFilesPage.tsx
@@ -38,7 +38,7 @@ const MyFilesPage = () => {
     const [breadcrumbsState, breadcrumbsDispatch] = useReducer(breadcrumbsReducer, {
         breadcrumbs: [{
             id: '',
-            name: 'Home'
+            name: 'My Files'
         }]
     });
     console.log('breadcrumbsState', breadcrumbsState)
