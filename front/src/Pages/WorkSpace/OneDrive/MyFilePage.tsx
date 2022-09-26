20bee2c3a97fbf9fe669b8af577fc3bb4325b1cb
Vidisha Arkasali <vidisha.a@tmax.in>
2022-09-26 19:29:03 +0530

    retre

---
+++ front/src/Pages/WorkSpace/OneDrive/MyFilePage.tsx
@@ -0,0 +1,100 @@
+import React,{useReducer} from 'react'
+import useCustom from '../../../hooks/useCustom'
+import { breadcrumbsReducer, foldersReducer } from '../../../Store copy/Reducer/foldersReducer';
+import { ActionType } from '../../../Store copy/Actions/actionTypes';
+import { useGetAllRootItemsOneDriveQuery, useGetItemChildrenOneDriveMutation } from '../../../services/graph'
+import MyFilesPage from '../../../Components/WorkSpaceOne/MyFilesPage'
+import { AuthenticatedTemplate } from '@azure/msal-react';
+import Breadcrumb from '../../../hooks/Breadcrumb';
+import { Grid } from '@mui/material';
+import { useStyles } from './Styles';
+const MyFilePage = () => {
+    const {token} = useCustom();
+    const classes = useStyles();
+    const { data, error, isLoading } = useGetAllRootItemsOneDriveQuery(token)
+    const [sendItem, { data: ItemChildren, error: itemChildrenError, isLoading: itemChildrenIsLoading }] = useGetItemChildrenOneDriveMutation();
+    console.log(data?.response)
+    const [breadcrumbsState, breadcrumbsDispatch] = useReducer(breadcrumbsReducer, {
+            breadcrumbs: [{
+                id: '',
+                name: 'My Files'
+            }]
+        });
+        console.log('breadcrumbsState', breadcrumbsState)
+        const setBreadCrumbAction = async (id: string, name: string) => {
+            return breadcrumbsDispatch({ type: ActionType.SET_BREADCRUMBS, payload: { id, name } });
+        };
+    
+        const updateBreadCrumbAction = async (id: string) => {
+            return breadcrumbsDispatch({ type: ActionType.UPDATE_BREADCRUMBS, payload: { id } });
+        };
+    const folderClickHandler = async (
+        // obj:any
+        id:string,
+        name:string,
+        folder:any,
+        webUrl: any
+    ): Promise<void> => {
+
+        // await getFolderChildrenAction(id);
+        // await setBreadCrumbAction(id, name);
+        console.log(id)
+        console.log(folder)
+        // console.log(webUrl)
+        if (folder === undefined) {
+            // setUrl(webUrl)
+
+            console.log('Not folder')
+        } else {
+            console.log('Its  folder')
+            // setShow(!show)
+            const Data = {
+                // name:id,
+                ItemId: id,
+                Name: name
+            }
+            //  console.log(fd)
+            await sendItem(Data)
+            await setBreadCrumbAction(id, name);
+        }
+
+
+        // setShow(!show)
+    };
+    const breadcrumbClickHandler = async (id: string) => {
+        await updateBreadCrumbAction(id);
+        // await getFolderChildrenAction(id);
+        const Data = {
+            // name:id,
+            ItemId: id,
+            //    Name:name
+        }
+        //  console.log(fd)
+         await sendItem(Data)
+    };
+  return (
+    <AuthenticatedTemplate>
+        <Grid>
+            <Grid className={classes.divFile}>
+                My Files
+            </Grid>
+            <Grid className={classes.bigPart}>
+         <Breadcrumb breadcrumb={breadcrumbsState.breadcrumbs}
+                            getChildHandler={breadcrumbClickHandler} />
+       <MyFilesPage
+         data={data}
+         error={error}
+         isLoading={isLoading}
+         ItemChildren={ItemChildren}
+         itemChildrenError={itemChildrenError}
+         itemChildrenIsLoading={itemChildrenIsLoading}
+         onClick={folderClickHandler}
+
+       />
+       </Grid>
+       </Grid>
+    </AuthenticatedTemplate>
+  )
+}
+
+export default MyFilePage
---
+++ front/src/Pages/WorkSpace/OneDrive/Styles.ts
@@ -0,0 +1,163 @@
+/* eslint-disable */
+import { makeStyles } from '@mui/styles';
+
+export const useStyles = makeStyles({
+  // topItems: {
+  //   display: "flex",
+  //   justifyContent: "space-around",
+
+  // },
+  // button: {
+  //   borderRadius: 14,
+  //   width: 80,
+  //   height: 50,
+  // },
+
+  // topClear: {
+  //   justifyContent: "end !important",
+  //   padding: "0 !important",
+  //   right: "15px",
+  // },
+  // topClearLink: {
+  //   display: "flex",
+  //   background: "#f3f3f3",
+  //   padding: "5px 9px",
+  //   borderRadius: "30px",
+  //   color: "#333333!important",
+  //   fontSize: "14px",
+  //   textDecoration: "none!important",
+  //   alignItems: "end",
+  //   "& svg": {
+  //     fontSize: "14px !important",
+  //     paddingLeft: "6px !important",
+  //   },
+  // },
+  // topLink: {
+  //   fontSize: "12px",
+  //   color: "#333333 !important",
+  //   textDecoration: "none !important",
+  //   "& .topImg": {
+  //     margin: "auto",
+  //     display: "block",
+  //   },
+  //   "& .topImgH": {
+  //     margin: "auto",
+  //     display: "none",
+  //   },
+  // },
+  // topMenu: {
+  //   "&:hover a .topImg": {
+  //     display: "none !important",
+  //   },
+  //   "&:hover a .topImgH": {
+  //     display: "block !important"
+  //   },
+
+  //   "&:hover a p": {
+  //     color: "#009BAD"
+  //   },
+  // },
+  // topText: {
+  //   margin: 0,
+  //   marginTop: "16px",
+  //   fontSize: "13px",
+  //   color: "#acacac",
+  // },
+
+  // linkPart: {
+  //   backgroundColor: "#33adff",
+  //   color: "white",
+  //   textTransform: "capitalize"
+  // },
+  // plus: {
+  //   textAlign: "center",
+  //   marginTop: "5px",
+  //   marginRight: "10px",
+  //   // backgroundColor:" white !important",
+  //   color: "white !important"
+  // },
+
+  // quick: {
+  //   marginTop: "5px",
+  //   marginRight: "10px"
+
+  // },
+  // myFile: {
+  //   display: "flex",
+  //   justifyContent: "space-between",
+
+  // },
+  // create: {
+  //   // backgroundColor: "rgb(50 168 189) !important",
+  //   color: "white !important",
+  //   marginRight: "15px !important",
+  //   padding: "3px 8px !important",
+
+
+
+  // },
+  // createNew: {
+  //   // backgroundColor: "rgb(50 168 189) !important",
+  //   color: "white !important",
+  //   // marginRight: "15px !important",
+  //   padding: "3px 8px !important",
+
+
+  // },
+
+  // linkBtn: {
+  //   backgroundColor: "rgb(50 168 189) !important",
+  //   color: "white !important",
+  //   marginRight: "30px !important",
+  //   padding: "5px 10px !important"
+  // },
+  bigPart: {
+    marginRight: "25px",
+    backgroundColor: "white",
+    marginTop: "20px",
+    borderRadius: "10px",
+  },
+  divFile: {
+    color: "#1b6189",
+    marginLeft: "20px",
+    marginTop: "20px",
+    textAlign: "left",
+
+  },
+  // menuImage: {
+  //   marginRight: "20px"
+  // },
+  // menu: {
+  //   marginTop: "15px"
+  // },
+
+  // customMenuPopover: {
+  //   // take note of !important because default z-index is applied inline
+  //   zIndex: "1900 !important"
+  // },
+
+  // lastMenu: {
+  //   marginTop: "107px",
+  //   backgroundColor: "#e9fbfb",
+  //   display: "flex",
+  //   justifyContent:"center",
+  //   padding: "10px",
+  //   width: "100px",
+  //   marginLeft:"77px"
+  // },
+  // items: {
+
+  //   fontSize: "12px",
+
+  //   "& img": {
+
+  //     width: "10px",
+
+  //     marginRight:"10px",
+
+  //   }
+
+  // }
+
+});
+
